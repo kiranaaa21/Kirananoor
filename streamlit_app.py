7ad4cc53656e4a7873779e810785782a387b4c80
kiranaaa21 <kirananooraprilianti@gmail.com>
2025-05-21 09:18:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Selamat Datang di Web Informatika!")
+st.title("Selamat Datang di Web Informatika")
 st.write(
     "ngoding seru bersama Bapak Hendri Setiadi"
 st.image("035d33428e946725de62bb6540d2c995.jpg", width= 200)
